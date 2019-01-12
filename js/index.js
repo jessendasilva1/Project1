d71f03db477a2cd9021075872c145ecb49b805d6
taylorfantin <EXAMPLE@example.com>
2019-01-12 13:37:34 -0500

    searchdiv

--- js/index.js
+++ js/index.js
@@ -1,18 +1,42 @@
-
-var city = "Vancouver";
-var apiKey = "IbzK4URLKfi6fOzYfdkwQCKu9b6BRBsU";
-var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?countryCode=CA&classificationName=music&City=" +city+"&apikey="+apiKey;
-
-console.log(queryURL);
-
-$.ajax ({
-    url: queryURL,
-    method: "GET",
-    async:true,
-    dataType: "json"
-}).then(function(response) {
-    console.log(response._embedded.events);
-
-    var results = response._embedded.events;
+//listen event to take search bar input and pass to the city variable
+$("#runSearch").on("click", function(event){
+    event.preventDefault();
+    var city = $("#searchBox").val().trim();
+    search(city);
 
 });
+
+//function to perform the city search and display top results
+function search(city){
+    $("#searchResults").empty();
+    var apiKey = "IbzK4URLKfi6fOzYfdkwQCKu9b6BRBsU";
+    var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&City="+city+"&apikey="+apiKey;
+        $.ajax ({
+            url: queryURL,
+            method: "GET",
+            async:true,
+            dataType: "json"
+        }).then(function(response) {
+
+            var results = response._embedded.events;
+            console.log(results);
+
+            for (var i = 0; i < results.length; i++){
+
+            var searchList = $("<ul>");
+            searchList.addClass("list-group");
+            $("#searchResults").append(searchList);
+            
+            var name = (results[i].name);
+            var date = (results[i].dates.start.localDate);
+            var venue = (results[i]._embedded.venues[0].name);
+        
+
+                var list = $("<li>");
+                list.text(date + " " + name + " " + venue);
+                $("#searchResults").append(list);
+
+            }
+        });
+}
+
