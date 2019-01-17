74cc32494e3c0c2c6481a7af16b41a6a6e362f52
Jessen <jessendasilva1@gmail.com>
2019-01-16 20:42:11 -0500

    new branch with session storage changes

--- js/page2.js
+++ js/page2.js
@@ -1,35 +1,58 @@
 //---------- Need : Get artist name from local storage -----------
-var artist = sessionStorage.getItem("artist");
-var artistIMG = sessionStorage.getItem("artistIMG");
-//----------------------------------------------------------------
-console.log(artist);
-console.log(artistIMG);
-
-var queryURL = "https://itunes.apple.com/search?term="+ artist +"&limit=6";
-var queryURL2 = "";
-
+var choice;
+var concert;
+var artist;
+var artistIMG;
 var artistName = "";
 var artistURL = "";
 var artistID = "";
 var country = "";
+var queryURL = "https://itunes.apple.com/search?term=";
+var limit = "&limit=6";
+var queryURL2 = "";
+//----------------------------------------------------------------
+console.log("initial: " + artist);
+//console.log(artistIMG);
 
-$.ajax({
-    url: queryURL,
-    dataType: 'JSONP', //This gets a JSON object instead of the default UTF8 result.
-    method: "GET"
-}).then(function (response) {
-    console.log(response.results);
-    var results = response.results;
+function test() {
+    choice = JSON.parse(sessionStorage.getItem("choice"));
+    concert = JSON.parse(sessionStorage.getItem("currentListOfConcerts"));
+    //console.log(concert[0]);
+    $("#artistName").text(concert[choice].name);
+    artist = concert[choice]._embedded.attractions[0].name;
+    $("#concertDateHeader").text(concert[choice].dates.start.localDate);
+    $("#concertLocationHeader").text(concert[choice]._embedded.venues[0].city.name + ", " + concert[choice]._embedded.venues[0].country.name);
+    console.log(artist);
+    call1();
+}
 
-    artistName = results[0].artistName;// Gets the Artist name for preview
-    artistURL = results[0].artistViewUrl;// Gets a link to the artists iTunes page
-    artistID = results[0].artistId;
-    country = response.results[0].country;// Gets the Artist Country for preview.
+function call1() {
+    console.log(artist);
+    queryURL = queryURL + artist + limit;
+    $.ajax({
+        url: queryURL,
+        dataType: 'JSONP', //This gets a JSON object instead of the default UTF8 result.
+        method: "GET"
+    }).then(function (response) {
+        console.log(response.results);
+        var results = response.results;
 
-    queryURL2 = "https://itunes.apple.com/lookup?id=" + artistID;//
+        artistName = results[0].artistName; // Gets the Artist name for preview
+        artistURL = results[0].artistViewUrl; // Gets a link to the artists iTunes page
+        artistID = results[0].artistId;
+        country = response.results[0].country; // Gets the Artist Country for preview.
 
-    call2();
-});
+        $("#pictureName").text(artistName);
+        $("#artistPicture").empty();
+        $("#artistPicture").append(`
+            <img id="${artist}" src="${concert[choice].images[0].url}" height="200px" width="200px">
+        `);
+
+        queryURL2 = "https://itunes.apple.com/lookup?id=" + artistID;
+
+        call2();
+    });
+}
 
 function call2() {
     $.ajax({
